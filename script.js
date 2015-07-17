ce561cab852dad4b2f4ef90b16ff9d4caae2acaf
Cole-de-Brito <coledebrito@gmail.com>
2015-07-17 18:53:59 +0000

    all done

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready( function(){
+  
+    // $(window).scroll( function(){
+  
+                   
+    //                 console.log("begin");
+    //$("#moon").delay(2000).css("background-color", "#FFE066").css("box-shadow:", "0px 0px 50px #FFFF00;");
+    //  console.log("finished!")
+  
+ 
+      $("#hi").click( function(){
+        $("#kanye").css( "opacity", "1");
+        
+      });
+      
+      
+      
+                 });
+                
+                
+  
+  
